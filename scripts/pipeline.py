2bddb842095474290b9396b369033a007a5a6951
Matt Franchi <mattwfranc@gmail.com>
2023-09-06 10:48:31 -0400

    pipeline script

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -9,33 +9,43 @@ import extract_clips as ec
 import logging
 
 logging.basicConfig(level=logging.INFO)
+PROCESSED_SUFFIX = 'noaud'
 
 def intersection_detection(source): 
     PROJECT_PATH = "/share/ju/xcpedestrian/urban-scene-understanding"
     YOLOV7_WEIGHTS = os.path.join(PROJECT_PATH, "yolov7/pretrained/yolov7x.pt")
-    INTERSECTION_COCO_CLASSES = [9, 11]
+    INTERSECTION_COCO_CLASSES = [0,1,2,3,4,5,6,7, 9, 11]
     TOP_LEVEL_DIR = os.path.join("intersection_detection")
     CDNET_WEIGHTS = os.path.join(PROJECT_PATH, "cdnet.pt")
 
-    stripped_source_path = re.sub(r'^[/\\.]+', '', source)
+    #stripped_source_path = re.sub(r'^[/\\.]+', '', source)
+    stripped_source_path = source
     logging.info(f'Processing source: {stripped_source_path}')
 
-    check_label_output_dir(stripped_source_path, f"{PROJECT_PATH}/scripts", TOP_LEVEL_DIR, "_coco", "_cdnet")
+    # remove audio from video file, downsample to 720p 
+    base, extension = os.path.splitext(source)
+    ffmpeg_cmd = f'ffmpeg -y -r 59.94 -i {source} -c:v copy -an {base}_{PROCESSED_SUFFIX}{extension}'
+    logging.info('Running ffmpeg')
+    subprocess.run(shlex.split(ffmpeg_cmd))
 
-    traffic_light_and_shadow_deweighting_cmd = f'python detect_shadow_deweighting.py --source {source} --weights {YOLOV7_WEIGHTS} --classes {" ".join(map(str, INTERSECTION_COCO_CLASSES))} --save-conf --save-txt --project {TOP_LEVEL_DIR} --name {os.path.splitext(os.path.basename(source))[0]}_coco'
+    check_label_output_dir(source, f"{PROJECT_PATH}/scripts", TOP_LEVEL_DIR, "_coco", "_cdnet")
+
+    traffic_light_and_shadow_deweighting_cmd = f'python detect_shadow_deweighting.py --source {base}_{PROCESSED_SUFFIX}{extension} --weights {YOLOV7_WEIGHTS} --img-size 640 --classes {" ".join(map(str, INTERSECTION_COCO_CLASSES))} --save-conf --save-txt --project {base}/{TOP_LEVEL_DIR} --name {os.path.splitext(os.path.basename(source))[0]}_coco'
+    logging.info(traffic_light_and_shadow_deweighting_cmd)
     logging.info('Running traffic light and shadow deweighting')
     subprocess.run(shlex.split(traffic_light_and_shadow_deweighting_cmd))
 
-    intersection_detection_cmd = f'python {PROJECT_PATH}/yolov7/detect.py --source {source} --weights {CDNET_WEIGHTS} --save-txt --save-conf --project {TOP_LEVEL_DIR} --name {os.path.splitext(os.path.basename(source))[0]}_cdnet'
+    intersection_detection_cmd = f'python {PROJECT_PATH}/yolov7/detect.py --source {base}_{PROCESSED_SUFFIX}{extension} --img-size 640 --weights {CDNET_WEIGHTS} --save-txt --save-conf --project {base}/{TOP_LEVEL_DIR} --name {os.path.splitext(os.path.basename(source))[0]}_cdnet'
+    logging.info(intersection_detection_cmd)
     logging.info('Running intersection detection')
     subprocess.run(shlex.split(intersection_detection_cmd))
 
     logging.info('Parsing periods')
-    periods = p.parse(stripped_source_path)
+    periods = p.parse(source, base, TOP_LEVEL_DIR, PROCESSED_SUFFIX)
     logging.info(f'Parsed periods: {periods}')
 
     logging.info('Extracting clips')
-    ec.extract_clips(f"{PROJECT_PATH}/{stripped_source_path}", periods, f"{PROJECT_PATH}/intersection_clips/{os.path.splitext(os.path.basename(source))[0]}")
+    ec.extract_clips(f"{source}", periods, f"{PROJECT_PATH}/intersection_clips/{os.path.splitext(os.path.basename(source))[0]}")
 
 def check_label_output_dir(source, proj_path, top_level_dir, coco_suffix, cdnet_suffix): 
     coco = os.path.exists(os.path.join(proj_path, top_level_dir, f"{os.path.splitext(os.path.basename(source))[0]}{coco_suffix}"))
@@ -47,7 +57,8 @@ def check_label_output_dir(source, proj_path, top_level_dir, coco_suffix, cdnet_
         logging.info(f"CDNET label output directory for {source} already exists")
     
     if coco or cdnet: 
-        erase = input("Erase existing label output directories? (y/n): ")
+        #erase = input("Erase existing label output directories? (y/n): ")
+        erase = 'y'
         if erase.lower() == "y":
             if coco: 
                 os.system(f"rm -rf {os.path.join(proj_path, top_level_dir, f'{os.path.splitext(os.path.basename(source))[0]}{coco_suffix}')}")
